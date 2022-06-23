00c7f66627211707499756c84d4a1b8acaf6a3f9
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-22 22:10:07 -0500

    middlewares/error: error handler for database

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,8 @@
+export class DatabaseConnectionError extends Error {
+  reason = 'Error connecting to databse';
+  constructor() {
+    super();
+    // Only because we are extending a built in class.
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+}
