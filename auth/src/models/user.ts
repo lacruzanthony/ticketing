73441b86c5b3f57a1e3ee85fc6b446525ea24ee6
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-25 22:40:58 -0500

    UserDoc interface added and some comments

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,29 @@
 import mongoose from 'mongoose';
 
+/**
+ * An interface that describes the properties
+ * that are required to create a new User.
+ */
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+/**
+ * An interface that descries the properties
+ * that a User Model has.
+ */
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+/**
+ * An interface that describes the properties
+ * that a User Document has.
+ */
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,6 +41,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
