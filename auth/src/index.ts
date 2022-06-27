ad8f940eb408a21c6190818d1e8df29b6da5d7e5
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-27 18:41:20 -0500

    setting the cookien config and trusting nginx proxy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -13,6 +14,13 @@ import { NotFoundError } from './errors/not-found-error';
 const app = express();
 
 app.use(json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
