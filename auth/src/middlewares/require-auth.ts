140aa6fc2dbb48223fb5f977cb81e45e80f33a8e
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-29 22:13:53 -0500

    adding a comment

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
-
+// Require auth middleware.
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!req.currentUser) {
     throw new NotAuthorizedError();
