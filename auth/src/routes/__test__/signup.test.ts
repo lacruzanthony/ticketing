0c13bc7e00659bb222c092e4f111d52c35628285
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-30 22:38:05 -0500

    adding test for other cases

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -4,3 +4,15 @@ import { app } from '../../app';
 it('returns a 201 on successful signup', async () => {
   return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app).post('/api/users/signup').send({ email: 'test', password: 'password' }).expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'p' }).expect(400);
+});
+
+it('returns a 400 with missing email password', async () => {
+  return request(app).post('/api/users/signup').send({}).expect(400);
+});
