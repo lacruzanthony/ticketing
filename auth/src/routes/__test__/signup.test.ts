895a2d82a546b95cbca64c9e57092b9132ec0b9a
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-06-30 22:47:33 -0500

    checking if the header for the cookie was created

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -21,3 +21,11 @@ it('disallows duplicate emails', async () => {
   await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
   await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(400);
 });
+
+it('sets a cookie after a successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
