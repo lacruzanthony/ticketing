206d26dcbbd96dd86681f0b5565a78bbfdd4256b
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-01 21:14:08 -0500

    test/currentUser: adding the cookie in the next upcoming request

--- auth/src/routes/__test__/current.test.ts
+++ auth/src/routes/__test__/current.test.ts
@@ -2,8 +2,14 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-  await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
+  const authResponse = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
 
-  const response = await request(app).get('/api/users/currentuser').send().expect(200);
-  console.log(response.body);
+  const cookie = authResponse.get('Set-Cookie');
+
+  const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200);
+
+  expect(response.body.currentUser.email).toEqual('test@test.com');
 });
