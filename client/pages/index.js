b92444e0acca946a8fb1b1c2508e9491dbe4c60b
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-03 20:46:46 -0500

    getting initial props from the auth namespace or from the auth service

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,11 +2,30 @@ import axios from 'axios';
 
 const Landing = ({ currentUser }) => {
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
-
   return <h1>Landing Page</h1>;
+};
+
+Landing.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server.
+    // request should be made to http://ingress-nginx...
+    const { data } = axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    );
+
+    console.log('on the server------------------');
+    return { data };
+  } else {
+    // we are on the browser.
+    // the base URL could be ''
+    const { data } = axios.get('/api/users/currentuser');
+    return { data };
+  }
 };
 
 export default Landing;
