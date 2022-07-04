8dac07dfc7fa37cf298514418191aa63d3b88cdb
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-03 21:05:31 -0500

    using template from video to void errors

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,32 @@
 import axios from 'axios';
 
-const Landing = ({ currentUser }) => {
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-Landing.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
-    // we are on the server.
-    // request should be made to http://ingress-nginx...
-    const { data } = axios.get(
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+    const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev'
-        }
+        headers: req.headers
       }
     );
 
-    console.log('on the server------------------');
-    return { data };
+    return data;
   } else {
-    // we are on the browser.
-    // the base URL could be ''
-    const { data } = axios.get('/api/users/currentuser');
-    return { data };
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
   }
 };
 
-export default Landing;
+export default LandingPage;
