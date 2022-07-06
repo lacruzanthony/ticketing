bda5414e7c4aa3c4c9c6066bc24cc7cd68863915
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-05 20:53:09 -0500

    referencing ticketsla

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken';
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
+
+import { BadRequestError, validateRequest } from '@ticketsla/common';
 
 const router = express.Router();
 
