83e48799cfbf32b6c3f0bd2d6e0fda3cd99c9913
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-06 22:20:49 -0500

    setting the cookie in the requests and adding TDD for price and title

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -12,11 +12,23 @@ it('can only be accessed if the user is signed in', async () => {
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
-  const response = await request(app).post('/api/tickets').send({});
+  const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({});
 
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app).post('/api/tickets').set('Cookie', global.signin()).send({ title: '', price: 20 }).expect(400);
+  await request(app).post('/api/tickets').set('Cookie', global.signin()).send({ price: 20 }).expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({ title: 'abcd', price: -10 })
+    .expect(400);
+  await request(app).post('/api/tickets').set('Cookie', global.signin()).send({ title: 'dasda' }).expect(400);
+});
 
 it('create a ticket with valid inputs', async () => {});
