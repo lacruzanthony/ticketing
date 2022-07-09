f7b92f5032ac546913a2c85f44523e0937f959e0
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-08 21:29:08 -0500

    adding updateticketrouter

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { NotFoundError, errorHandler, currentUser } from '@ticketsla/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
