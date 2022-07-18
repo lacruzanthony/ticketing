c5b016a2ea5262900982bd3f21ad2b8702da38d8
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-17 20:26:21 -0500

    orders src files

---
+++ orders/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, errorHandler, currentUser } from '@ticketsla/common';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+
+const app = express();
+
+app.use(json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
