238335d67e7a80773bad9ef098a04e30e5d99c74
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-23 21:01:49 -0500

    emiting event when the order is created

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import express, { Request, Response } from 'express';
 import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@ticketsla/common';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
@@ -48,6 +50,16 @@ router.post(
     await order.save();
 
     // Publish an event saying that an order was created.
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price
+      }
+    });
 
     res.status(201).send(order);
   }
