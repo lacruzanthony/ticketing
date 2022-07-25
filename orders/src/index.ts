4cd528b3d78c7b1ffa16286ced680b4ca91880f7
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-25 16:22:39 -0500

    initializing the tickets' listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 import { app } from './app';
 
@@ -28,6 +30,9 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('connected to mongodb');
