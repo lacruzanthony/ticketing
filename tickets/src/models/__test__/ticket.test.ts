f4223e6bc1cbe77b04017202bb77007e7f16ed10
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-27 22:32:34 -0500

    testing version on each mongoose save

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -20,3 +20,15 @@ it('implements optimistic concurrency control', async () => {
     return;
   }
 });
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({ title: 'concert', price: 5, userId: '123' });
+
+  await ticket.save();
+
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
