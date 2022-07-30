305bdc4d1407b95e85b326f8f09e2af1613810be
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-07-29 21:09:23 -0500

    adding the version prop on CRUD

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@ticketsla/common';
 import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@ticketsla/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
@@ -18,15 +18,18 @@ router.post(
   async (req: Request, res: Response) => {
     const { title, price } = req.body;
 
-    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
-
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    });
     await ticket.save();
-
     new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
-      userId: ticket.userId
+      userId: ticket.userId,
+      version: ticket.version
     });
 
     res.status(201).send(ticket);
