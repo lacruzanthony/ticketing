6e6128e32e5e75e3a63458ca0d32523235910da9
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-08-12 22:12:17 -0500

    nats wrapper for payments

---
+++ payments/src/nats-wrapper.ts
@@ -0,0 +1,29 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  // client
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+    return this._client;
+  }
+
+  connect(clusterId: string, cliendId: string, url: string) {
+    this._client = nats.connect(clusterId, cliendId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this.client.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
