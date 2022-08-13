dbff5146d042b9473cc57bd83554be0d19761320
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-08-12 22:11:58 -0500

    app file

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, errorHandler, currentUser } from '@ticketsla/common';
+
+const app = express();
+
+app.use(json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
