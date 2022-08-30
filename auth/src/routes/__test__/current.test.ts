b7e959e87eb7a4bff269177596c82b6a33a3d686
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-08-29 23:10:44 -0500

    Fixing Unit testing

--- auth/src/routes/__test__/current.test.ts
+++ auth/src/routes/__test__/current.test.ts
@@ -4,7 +4,7 @@ import { app } from '../../app';
 it('responds with details about the current user', async () => {
   const cookie = await global.signin();
 
-  const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(400);
+  const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
